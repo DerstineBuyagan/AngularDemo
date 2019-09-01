75eb07ee161068fcacbd067edea48e2abd08eed1
Derstine Buyagan <bastedboyderzthine@gmail.com>
2019-09-01 22:10:39 +0800

    xf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ProductAlertComponent } from "./product-alert/product-alert.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAlertComponent,
+    ProductListComponent,
+    TopBarComponent,
+    ProductDetailsComponent,
+    ProductAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
